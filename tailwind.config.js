48a5cb911a7bffb194d1f4bebd617ebc69447c46
WarFreak-ATCS <danrebbjohnorolfo@gmail.com>
2024-07-11 07:58:03 +0800

    dffd

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        dosis: "Dosis",
+        source: "Source"
+      },
+      colors: {
+        dark: "#000000",
+        light: "#ffffff",
+        accent: "#123a54",
+      },
+    },
+  },
+  plugins: [],
+}
+
